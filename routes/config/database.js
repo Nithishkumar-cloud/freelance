66e1a8ac14fb37c8a6513a33e62d543848e2475c
Nithish <84829934+Nithishkumar-cloud@users.noreply.github.com>
2023-08-29 13:00:51 +0530

    Update database.js

--- routes/config/database.js
+++ routes/config/database.js
@@ -3,7 +3,7 @@
 const mongoose= require('mongoose');
 
 const connectDatabase=()=>{
-    mongoose.connect(process.env.DB_LOCAL_URI,{
+    mongoose.connect(process.env.DATABASE_LOCAL_URI,{
         useNewUrlParser:true,//--->in older version the connection string (DB_LOCAL-URI=mongodb://localhost:27017/nithishcart) is processed in different parser method but we dont need to use older version so here we have used useNewUrlParser:true --->it say use new parser dont use older one..
         useUnifiedTopology:true,//--->mongodb driver ku palaya options neraya irunthuchu but namma intha new version la antha options la use panna kudathu so  useUnifiedTopology:true-->nu kudukuron ipo ula new version la irukura options than use pannanum nu soldren...
         dbName: 'huh'
