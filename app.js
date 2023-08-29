cb32f09141fbd0e291edb748e9f7911d5f17dc15
Nithish <84829934+Nithishkumar-cloud@users.noreply.github.com>
2023-08-29 13:28:22 +0530

    Update app.js

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.use(express.json());
 app.set("views", path.join(__dirname, '../views'));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + '/public/'));
-app.use('/uploads',express.static(path.join(__dirname,'./routes/uploads')));
+//app.use('/uploads',express.static(path.join(__dirname,'./routes/uploads')));
 
 //app.use((req, res, next) => {
 //  res.setHeader("Access-Control-Allow-Origin", "*");
